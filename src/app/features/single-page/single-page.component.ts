e42b09359805051cc8820bf94acd2bc9f7910dfd
KTsarenko <konstantin.tsarenko@gmail.com>
2019-05-10 23:45:19 +0300

    up gallery

--- src/app/features/single-page/single-page.component.ts
+++ src/app/features/single-page/single-page.component.ts
@@ -3,6 +3,7 @@ import {RequestService} from '../../core/services/request.service';
 import {Router} from '@angular/router';
 import {RequestModel} from '../../core/models/request.model';
 import {GalleryComponent} from '../../shared/gallery/gallery.component';
+import {NgxGalleryImage, NgxGalleryOptions} from 'ngx-gallery';
 
 @Component({
     selector: 'app-single-page',
@@ -19,6 +20,8 @@ import {GalleryComponent} from '../../shared/gallery/gallery.component';
 export class SinglePageComponent implements OnInit {
 
     public request: any;
+    galleryOptions: NgxGalleryOptions[];
+    galleryImages: NgxGalleryImage[];
 
     constructor(private _requestService: RequestService,
                 private router: Router) {
@@ -28,13 +31,40 @@ export class SinglePageComponent implements OnInit {
         const [blank, container, collection, id] = this.router.url.split('/');
         this.getSingleRequest(id, collection);
         this.request = {};
+
+        this.galleryOptions = [
+            {
+                "image": false,
+                "thumbnailsRemainingCount": true,
+                "height": "150px"
+            },
+            {
+                "breakpoint": 500,
+                "width": "100%",
+                "thumbnailsColumns": 2
+            }
+        ]
+
+        this.galleryImages = [
+            {
+                small: 'https://firebasestorage.googleapis.com/v0/b/serviceplace-kt.appspot.com/o/images%2Fd1cd1117-4a59-442c-8ce8-927a30406a11.jpg?alt=media&token=deaae9cf-e1d5-496a-a006-649354de769e',
+                medium: 'https://firebasestorage.googleapis.com/v0/b/serviceplace-kt.appspot.com/o/images%2Fd1cd1117-4a59-442c-8ce8-927a30406a11.jpg?alt=media&token=deaae9cf-e1d5-496a-a006-649354de769e',
+                big: 'https://firebasestorage.googleapis.com/v0/b/serviceplace-kt.appspot.com/o/images%2Fd1cd1117-4a59-442c-8ce8-927a30406a11.jpg?alt=media&token=deaae9cf-e1d5-496a-a006-649354de769e'
+            },
+            {
+                small: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg',
+                medium: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg',
+                big: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg'
+            }
+        ];
+
     }
 
     private getSingleRequest(id, collection): void {
         this._requestService.getSingleRequest(id, collection)
             .then(doc => {
                 if (doc.exists) {
-                    // console.log('Document data:', doc.data());
+                    console.log('Document data:', doc.data());
                     this.request = doc.data();
                 } else {
                     console.log('No such document!');
--- src/app/shared/gallery/gallery.component.ts
+++ src/app/shared/gallery/gallery.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation} from 'ngx-gallery';
+// import {NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation} from 'ngx-gallery';
 
 @Component({
     selector: 'app-gallery',
@@ -8,35 +8,8 @@ import {NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation} from 'ngx-galle
 })
 
 export class GalleryComponent implements OnInit {
-    galleryOptions: NgxGalleryOptions[];
-    galleryImages: NgxGalleryImage[];
 
     ngOnInit(): void {
 
-        this.galleryOptions = [
-            {
-                "image": false,
-                "thumbnailsRemainingCount": true,
-                "height": "100px"
-            },
-            {
-                "breakpoint": 500,
-                "width": "100%",
-                "thumbnailsColumns": 2
-            }
-        ]
-
-        this.galleryImages = [
-            {
-                small: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg',
-                medium: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg',
-                big: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg'
-            },
-            {
-                small: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg',
-                medium: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg',
-                big: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg'
-            }
-        ];
     }
 }
