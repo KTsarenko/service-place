7ae490a47fef62aa6a108b259f17e5829f174d0b
KTsarenko <konstantin.tsarenko@gmail.com>
2019-05-18 11:23:41 +0300

    up gallery

--- src/app/features/single-page/single-page.component.ts
+++ src/app/features/single-page/single-page.component.ts
@@ -1,9 +1,8 @@
-import {Component, NgModule, OnInit} from '@angular/core';
-import {RequestService} from '../../core/services/request.service';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {RequestModel} from '../../core/models/request.model';
-import {GalleryComponent} from '../../shared/gallery/gallery.component';
-import {NgxGalleryImage, NgxGalleryOptions} from 'ngx-gallery';
+
+import {RequestService} from '../../core/services/request.service';
+
 
 @Component({
     selector: 'app-single-page',
@@ -11,66 +10,60 @@ import {NgxGalleryImage, NgxGalleryOptions} from 'ngx-gallery';
     styleUrls: ['./single-page.component.scss']
 })
 
-// @NgModule({
-//     imports: [
-//         GalleryComponent
-//     ]
-// })
-
 export class SinglePageComponent implements OnInit {
 
     public request: any;
-    galleryOptions: NgxGalleryOptions[];
-    galleryImages: NgxGalleryImage[];
+    public galleryOptions = [];
+    public galleryImages = [];
 
     constructor(private _requestService: RequestService,
                 private router: Router) {
+        this.galleryOptions = [
+            {
+                'image': false,
+                'thumbnailsRemainingCount': true,
+                'height': '150px'
+            },
+            {
+                'breakpoint': 500,
+                'width': '100%',
+                'thumbnailsColumns': 2
+            }
+        ];
     }
 
     ngOnInit() {
         const [blank, container, collection, id] = this.router.url.split('/');
         this.getSingleRequest(id, collection);
         this.request = {};
-
-        this.galleryOptions = [
-            {
-                "image": false,
-                "thumbnailsRemainingCount": true,
-                "height": "150px"
-            },
-            {
-                "breakpoint": 500,
-                "width": "100%",
-                "thumbnailsColumns": 2
-            }
-        ]
-
-        this.galleryImages = [
-            {
-                small: 'https://firebasestorage.googleapis.com/v0/b/serviceplace-kt.appspot.com/o/images%2Fd1cd1117-4a59-442c-8ce8-927a30406a11.jpg?alt=media&token=deaae9cf-e1d5-496a-a006-649354de769e',
-                medium: 'https://firebasestorage.googleapis.com/v0/b/serviceplace-kt.appspot.com/o/images%2Fd1cd1117-4a59-442c-8ce8-927a30406a11.jpg?alt=media&token=deaae9cf-e1d5-496a-a006-649354de769e',
-                big: 'https://firebasestorage.googleapis.com/v0/b/serviceplace-kt.appspot.com/o/images%2Fd1cd1117-4a59-442c-8ce8-927a30406a11.jpg?alt=media&token=deaae9cf-e1d5-496a-a006-649354de769e'
-            },
-            {
-                small: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg',
-                medium: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg',
-                big: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg'
-            }
-        ];
-
     }
 
     private getSingleRequest(id, collection): void {
         this._requestService.getSingleRequest(id, collection)
             .then(doc => {
                 if (doc.exists) {
-                    this.request = doc.data();
+                    const data = doc.data();
+                    this.request = data;
+                    this.galleryImages = this.setGalleryImg(data.images);
+                    // console.log(this.request);
                 } else {
                     console.log('No such document!');
                 }
-            }).catch(function(error) {
+            }).catch(function (error) {
             console.error('Error getting document:', error);
         });
     }
 
+    private setGalleryImg(imgArr): Array<Object> {
+        const galleryImgArr = [];
+        imgArr.forEach(img => {
+            const galleryImg = {
+                small: img.url,
+                medium: img.url,
+                big: img.url
+            };
+            galleryImgArr.push(galleryImg);
+        });
+        return galleryImgArr;
+    }
 }
