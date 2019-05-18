58ff7030502519042414d2b12f2169e4d8f6f078
KTsarenko <konstantin.tsarenko@gmail.com>
2019-05-18 10:23:17 +0300

    up home

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -33,15 +33,14 @@ export class ProfileComponent implements OnInit {
     this._fb.getProfile()
       .then(doc => {
         if (doc.exists) {
-          console.log("Document data:", doc.data());
           this.request = doc.data();
           this.fillForm();
         } else {
           // doc.data() will be undefined in this case
-          console.log("No such document!");
+          console.log('No such document!');
         }
       }).catch(function (error) {
-      console.log("Error getting document:", error);
+      console.log('Error getting document:', error);
     });
   }
 
